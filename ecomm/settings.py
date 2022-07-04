6873aee0e216eb40cd274569a9ae53f108c35c08
d-imosemi <83042447+d-imosemi@users.noreply.github.com>
2022-07-05 00:22:37 +0100

    modified settings

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -169,7 +169,9 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
